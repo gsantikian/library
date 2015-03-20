fb1732187a6a4deb3978eb678534f2e8f4f39540
Greg Santikian <gsantikian@gmail.com>
2015-03-20 11:39:59 -0400

    Adds clear function, cleans up user experience, slight refactor to removeBook

--- library.js
+++ library.js
@@ -23,7 +23,6 @@ Library.prototype.addBook = function(book) {
 Library.prototype.removeBook = function(bookTitle) {
   for (var i = 0, len = this.books.length; i < len; i++) {
     if (this.books[i].title === bookTitle) {
-      console.log(this.books[i].title + " has been removed.");
       this.books.splice(i, 1);
       break;
     }
@@ -31,9 +30,9 @@ Library.prototype.removeBook = function(bookTitle) {
 };
 
 Library.prototype.displayBooks = function() {
-  var propertyName = arguments[0];
-  var value = arguments[1];
-  if (arguments.length > 0) {
+  if (arguments.length) {
+    var propertyName = arguments[0];
+    var value = arguments[1];
     this.books.forEach(function(book) {
       if (book[propertyName] === value) {
         book.printBookDetails();
@@ -44,7 +43,12 @@ Library.prototype.displayBooks = function() {
       book.printBookDetails();
     });
   }
+  console.log("\n");
 };
+
+function clear() {
+  console.log('\033[2J');
+}
 
 function userInput(str) {
   return sget(str).trim();
@@ -63,10 +67,9 @@ library.addBook(shortHistory);
 library.addBook(something);
 
 var quit = false;
+console.log("Library Inventory System");
 
 while (!quit) {
-  console.log(library);
-  console.log("Library Inventory System");
   console.log("Please select from the following options:\n1. Add book to inventory\n2. Remove book from inventory\n3. List all books\n4. List books by genre\n5. Search by title\n6. Search by author\n7. Quit");
   var userSelection = userInput();
 
@@ -78,30 +81,37 @@ while (!quit) {
       var genre = userInput("Enter genre:");
       var pages = userInput("Enter number of pages:");
       library.addBook(new Book(title, author, genre, pages));
+      clear();
       console.log(title + " added to inventory.");
       break;
     case '2':
       console.log("Remove book");
       var bookTitle = userInput("Enter title of book to remove:");
       library.removeBook(bookTitle);
+      clear();
+      console.log(bookTitle + " removed.");
       break;
     case '3':
+      clear();
       console.log("All books:");
       library.displayBooks();
       break;
     case '4':
       console.log("Books by genre:");
       genre = userInput("Enter genre to search by:");
+      clear();
       library.displayBooks("genre", genre);
       break;
     case '5':
       console.log("Search by title");
       title = userInput("Enter title:");
+      clear();
       library.displayBooks("title", title);
       break;
     case '6':
       console.log("Search by author");
       author = userInput("Enter author:");
+      clear();
       library.displayBooks("author", author);
       break;
     case '7':
@@ -109,6 +119,7 @@ while (!quit) {
       quit = true;
       break;
     default:
+      clear();
       console.log("Invalid input. Please try again by selecting #1-6");
   }
 }
